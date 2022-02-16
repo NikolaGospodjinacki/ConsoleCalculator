d22a3bcee92f7ccbd496ff2c66bd12243b4c9b8b
Nikola Gospodjinacki <ngogank@gmail.com>
2022-02-16 16:25:13 +0100

    Add project files.

---
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProgram
+{
+   public class CalculatorLibrary
+    {
+        public static double CalculateValue(double firstNumber, double secondNumber, string operation)
+        {
+            if (firstNumber != 0 && secondNumber != 0)
+            {
+                return operation switch
+                {
+                    "a" => firstNumber + secondNumber,
+                    "d" => firstNumber - secondNumber,
+                    "m" => firstNumber * secondNumber,
+                    "e" => firstNumber / secondNumber,
+                    _ => double.NaN
+                };
+            }
+            else return double.NaN;
+        }
+    }
+}
---
+++ Program/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using CalculatorProgram;
+
+namespace CaculatorProgram
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            bool IsOver = false;
+            while (!IsOver)
+            {
+                string firstInput = "";
+                string secondInput = "";
+                string @operator = "";
+                double result = 0;
+
+                Console.WriteLine("This is a basic command line calculator");
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine("Enter first number:");
+                firstInput = Console.ReadLine();
+
+                //Converts input to double, checks if the input is correct
+                double firstClean = 0;
+                while (!double.TryParse(firstInput, out firstClean))
+                {
+                    Console.WriteLine("Input for the first number is not valid, try again:\n");
+                    firstInput = Console.ReadLine();
+                }
+
+                Console.WriteLine("Enter second number");
+                secondInput = Console.ReadLine();
+
+                double secondClean = 0;
+                while (!double.TryParse(secondInput, out secondClean))
+                {
+                    Console.WriteLine("Input for the second number is not valid, try again:\n");
+                    secondInput = Console.ReadLine();
+                }
+
+                Console.WriteLine("Available operations:");
+                Console.WriteLine("\ta - Addition");
+                Console.WriteLine("\td - Deduction");
+                Console.WriteLine("\tm - Multiplication");
+                Console.WriteLine("\te - Division");
+                Console.WriteLine("Your choice?\n");
+               
+                @operator = Console.ReadLine();
+                try
+                {
+                    result = CalculatorLibrary.CalculateValue(firstClean, secondClean, @operator);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("Calculation is not valid");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Result for {0} and {1} is: {2}", firstClean, secondClean, result);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Something went wrong, please try again. Details:" + e);
+                }
+
+                Console.WriteLine("Do you want to do another calculation?");
+                Console.WriteLine("Press n to exit, or anything else to continue.");
+
+                if (Console.ReadLine() == "n")
+                {
+                    IsOver = true;
+                    return;
+                }
+            }
+
+        }
+    }
+}
